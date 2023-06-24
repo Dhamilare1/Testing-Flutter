58d57ac9affdef3d694129b461fe44f410387c25
temitope <aladesiuntope@gmail.com>
2023-06-24 14:53:59 +0100

    updates

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -5,17 +5,42 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-    width: 428,
-    height: 926,
-    clipBehavior: Clip.antiAlias,
-    decoration: const BoxDecoration(color: Color(0xFFFEFDFA)),
-    child: Stack(
+   
+    return Container(
+      width: 428,
+      height: 926,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(color: Color(0xFFFEFDFA)),
+      child: Stack(
         children: [
-          
-          
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 100,
+                  ),
+                  Column(
+                    children: [
+                      Text('Dams', style:Theme.of(context).textTheme.headlineLarge?.copyWith(color: Colors.black, fontSize: 35, fontWeight: FontWeight.bold),),
+                      Text('....Save a Life', style:Theme.of(context).textTheme.headlineLarge?.copyWith(color: const Color(0xff616161), fontSize: 12))],
+                  )
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+              right: 0,
+              bottom: 0,
+              child: Image.asset(
+                'assets/images/bigheart.png',
+                height: 250,
+              ))
         ],
-    ),
-);
+      ),
+    );
   }
 }
